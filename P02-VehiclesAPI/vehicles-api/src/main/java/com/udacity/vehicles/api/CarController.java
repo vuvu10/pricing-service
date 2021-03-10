03b8b2618e8ee89cf170e33ef776dd2d54ac3f22
dcsiona@gmail.com <dcsiona@gmail.com>
2021-03-10 12:28:07 -0500

    Implementing all the TODOS for the CarController

--- P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -58,11 +58,7 @@ class CarController {
      */
     @GetMapping("/{id}")
     Resource<Car> get(@PathVariable Long id) {
-        /**
-         * TODO: Use the `findById` method from the Car Service to get car information.
-         * TODO: Use the `assembler` on that car and return the resulting output.
-         *   Update the first line as part of the above implementing.
-         */
+
         Car car = carService.findById(id);
         return assembler.toResource(new Car());
     }
@@ -75,11 +71,7 @@ class CarController {
      */
     @PostMapping
     ResponseEntity<?> post(@Valid @RequestBody Car car) throws URISyntaxException {
-        /**
-         * TODO: Use the `save` method from the Car Service to save the input car.
-         * TODO: Use the `assembler` on that saved car and return as part of the response.
-         *   Update the first line as part of the above implementing.
-         */
+
         Car newCar = carService.save(car);
 
         Resource<Car> resource = assembler.toResource(new Car());
@@ -94,12 +86,7 @@ class CarController {
      */
     @PutMapping("/{id}")
     ResponseEntity<?> put(@PathVariable Long id, @Valid @RequestBody Car car) {
-        /**
-         * TODO: Set the id of the input car object to the `id` input.
-         * TODO: Save the car using the `save` method from the Car service
-         * TODO: Use the `assembler` on that updated car and return as part of the response.
-         *   Update the first line as part of the above implementing.
-         */
+
         car.setId(id);
         Car updatedCar = carService.save(car);
         Resource<Car> resource = assembler.toResource(new Car());
@@ -113,9 +100,7 @@ class CarController {
      */
     @DeleteMapping("/{id}")
     ResponseEntity<?> delete(@PathVariable Long id) {
-        /**
-         * TODO: Use the Car Service to delete the requested vehicle.
-         */
+
         carService.delete(id);
         return ResponseEntity.noContent().build();
     }
